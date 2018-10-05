c73beb23250f3b6ce4542338cf0c810e24602ec0
Emily <emily.birch@infinityworks.com>
2018-10-05 11:09:22 +0100

    Username field removed from DB create script and DB schema desciption

--- DBCreateScript.py
+++ DBCreateScript.py
@@ -29,7 +29,6 @@ cursor.execute("USE snailRacing")
 cursor.execute("CREATE TABLE users (userID INT AUTO_INCREMENT PRIMARY KEY,"
                                     "firstName VARCHAR(50),"
                                     "lastName VARCHAR(50),"
-                                    "username VARCHAR(50),"
                                     "password VARCHAR(255),"
                                     "email VARCHAR(50))")
 
