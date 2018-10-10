58d831882f07d2bd0240097256b8094327dca218
ashwallsinfinity <ashley.walls@infinityworks.com>
2018-10-10 10:56:59 +0100

    changed mysql syntax to set up postgres. Tested and all tables are added to snailRacing database on vagrant logged in as root

--- DBCreateScript.py
+++ DBCreateScript.py
@@ -1,98 +1,80 @@
 # import mysql.connector
 import psycopg2
 
-# connect to local MySQL databases
-#db = mysql.connector.connect(host="localhost", user="root", passwd="mysqlpasswd")
-#cursor = db.cursor()
-
-# connect to local PostgreSQL database
 
 try:
     db = psycopg2.connect("host='localhost' dbname='snailRacing' user='root' password='psqlpass'")
     cursor = db.cursor()
-    cursor.execute("DROP DATABASE snailRacing")
+
 except:
-    pass
+    print("I am unable to connect to the database")
 
-try:
-    db = psycopg2.connect("host='localhost' dbname='snailRacing' user='root' password='psqlpass'")
-    cursor = db.cursor()
-    cursor.execute("CREATE DATABASE snailRacing")
-except:
-    pass
-
-# db.commit() "saves" changes to the DB
-#db.commit()
-
-# the connect statement from earlier essentially points to all local DBs that are present
-# -- need to tell MySQL to use the snail racing DB specifically, with "USE"
-#cursor.execute("USE snailRacing")
 
 
 # create user and user score tables
-cursor.execute("CREATE TABLE users (userID INT AUTO_INCREMENT PRIMARY KEY,"
+cursor.execute("CREATE TABLE users (userID serial PRIMARY KEY,"
                                     "firstName VARCHAR(50),"
                                     "lastName VARCHAR(50),"
                                     "password VARCHAR(50),"
-                                    "email VARCHAR(100))")
+                                    "email VARCHAR(100));")
 
-cursor.execute("CREATE TABLE userOverallScore (ID INT AUTO_INCREMENT PRIMARY KEY,"
+cursor.execute("CREATE TABLE userOverallScore (ID serial PRIMARY KEY,"
                                                 "userID INT,"
                                                 "score INT,"
-                                                "FOREIGN KEY(userID) REFERENCES users(userID))")
+                                                "FOREIGN KEY(userID) REFERENCES users(userID));")
 
 
 # create trainer and snails tables
-cursor.execute("CREATE TABLE trainers (trainerID INT AUTO_INCREMENT PRIMARY KEY,"
-                                        "name VARCHAR(50))")
+cursor.execute("CREATE TABLE trainers (trainerID serial PRIMARY KEY,"
+                                        "name VARCHAR(50));")
 
-cursor.execute("CREATE TABLE snails (snailID INT AUTO_INCREMENT PRIMARY KEY,"
+cursor.execute("CREATE TABLE snails (snailID serial PRIMARY KEY,"
                                     "trainerID INT,"
                                     "name VARCHAR(50),"
-                                    "FOREIGN KEY(trainerID) REFERENCES trainers(trainerID))")
+                                    "FOREIGN KEY(trainerID) REFERENCES trainers(trainerID));")
 
 
 # create round and race tables
-cursor.execute("CREATE TABLE round (roundID INT AUTO_INCREMENT PRIMARY KEY,"
+cursor.execute("CREATE TABLE round (roundID serial PRIMARY KEY,"
                                     "prize VARCHAR(50),"
-                                    "start DATETIME,"
-                                    "finish DATETIME,"
-                                    "roundName VARCHAR(20))")
+                                    "start TIMESTAMP,"
+                                    "finish TIMESTAMP,"
+                                    "roundName VARCHAR(20));")
 
-cursor.execute("CREATE TABLE race (raceID INT AUTO_INCREMENT PRIMARY KEY,"
+cursor.execute("CREATE TABLE race (raceID serial PRIMARY KEY,"
                                     "roundID INT,"
-                                    "raceDate DATETIME,"
+                                    "raceDate TIMESTAMP,"
                                     "status INT,"
-                                    "FOREIGN KEY(roundID) REFERENCES round(roundID))")
+                                    "FOREIGN KEY(roundID) REFERENCES round(roundID));")
 
 
 # create racecard, race predictions, and race result tables
-cursor.execute("CREATE TABLE racecard (racecardID INT AUTO_INCREMENT PRIMARY KEY,"
+cursor.execute("CREATE TABLE racecard (racecardID serial PRIMARY KEY,"
                                         "raceID INT,"
                                         "snailID INT,"
                                         "FOREIGN KEY(raceID) REFERENCES race(raceID), "
-                                        "FOREIGN KEY(snailID) REFERENCES snails(snailID))")
+                                        "FOREIGN KEY(snailID) REFERENCES snails(snailID));")
 
 
-cursor.execute("CREATE TABLE raceResult (raceResultID INT AUTO_INCREMENT PRIMARY KEY,"
+cursor.execute("CREATE TABLE raceResult (raceResultID serial PRIMARY KEY,"
                                             "raceID INT,"
                                             "snailID INT,"
                                             "position INT,"
                                             "FOREIGN KEY(raceID) REFERENCES race(raceID),"
-                                            "FOREIGN KEY(snailID) REFERENCES snails(snailID))")
+                                            "FOREIGN KEY(snailID) REFERENCES snails(snailID));")
 
-cursor.execute("CREATE TABLE racePredictions (racePredID INT AUTO_INCREMENT PRIMARY KEY,"
+cursor.execute("CREATE TABLE racePredictions (racePredID serial PRIMARY KEY,"
                                             "raceID INT,"
                                             "userID INT,"
                                             "snailID INT,"
-                                            "created DATETIME,"
+                                            "created TIMESTAMP,"
                                             "FOREIGN KEY(raceID) REFERENCES race(raceID),"
                                             "FOREIGN KEY(userID) REFERENCES users(userID),"
-                                            "FOREIGN KEY(snailID) REFERENCES snails(snailID))")
+                                            "FOREIGN KEY(snailID) REFERENCES snails(snailID));")
 
 # create status tables
-cursor.execute("CREATE TABLE status(statusID INT AUTO_INCREMENT PRIMARY KEY,"
-                                    "status VARCHAR(50))")
+cursor.execute("CREATE TABLE status(statusID serial PRIMARY KEY,"
+                                    "status VARCHAR(50));")
 
 # commit all changes to the DB
 db.commit()
