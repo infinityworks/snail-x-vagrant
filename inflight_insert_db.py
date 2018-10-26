c1749499b81d899cb5c1d1f573d2b80e64fb28d7
matt-twomey-iw <matt.twomey@gmail.com>
2018-10-26 16:48:34 +0100

    added raceresults to inflght insert db script

--- inflight_insert_db.py
+++ inflight_insert_db.py
@@ -19,13 +19,18 @@ cursor.execute("INSERT INTO snails (snail_id, trainer_id, name) "
 
 # Adding races
 cursor.execute("INSERT INTO race (race_id, round_id, race_date) "
-               "VALUES (4, 2, '2018-10-18 12:00:00'), (5, 2, '2018-10-19 12:00:00'), (6, 2, '2019-10-20 12:00:00');")
+               "VALUES (1, 2, '2018-10-18 12:00:00'), (2, 2, '2018-10-19 12:00:00'), (6, 2, '2019-10-20 12:00:00');")
 
 # Adding racecards
 cursor.execute("INSERT INTO racecard (race_card_id, race_id, snail_id) "
                "VALUES (1, 6, 1), (2, 6, 2), (3, 6, 3), "
-               "       (7, 4, 2), (8, 4, 6), (9, 4, 5), "
-               "        (4, 5, 4), (5, 5, 5), (6, 5, 1);")
+               "       (7, 1, 2), (8, 1, 6), (9, 1, 5), "
+               "        (4, 2, 4), (5, 2, 5), (6, 2, 1);")
+
+# Adding raceresults
+cursor.execute("INSERT INTO raceresult (race_result_id, race_id, snail_id, position) "
+               "VALUES (1, 6, 1, 1), (2, 6, 2, 2), (3, 6, 3, 3), "
+               "       (7, 1, 2, 3), (8, 1, 6, 2), (9, 1, 5, 1);")
 
 # commit all changes to the DB
 db.commit()
