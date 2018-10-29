fef219b1470964e4f03a19adf1cfc304aea2a7e7
matt-twomey-iw <matt.twomey@gmail.com>
2018-10-29 16:29:36 +0000

    Infllight updated to allow for consume result data testing

--- inflight_insert_db.py
+++ inflight_insert_db.py
@@ -17,20 +17,19 @@ cursor.execute("INSERT INTO snails (snail_id, trainer_id, name) "
                "(3, 3, 'Christian Snale'), (4, 4, 'Slime Shady'), (5, 5, 'Snailee Steinfeld'),"
                "(6, 2, 'Mi-shell Obama');")
 
-# Adding races
+# Adding races (2 races  that occurred in the past, one of which has a sresult recorded for it. The test result json
+# data provides the result for race 2, and the round is then changed to closed.
 cursor.execute("INSERT INTO race (race_id, round_id, race_date) "
-               "VALUES (1, 2, '2018-10-18 12:00:00'), (2, 2, '2018-10-19 12:00:00'), (6, 2, '2019-10-20 12:00:00');")
+               "VALUES (1, 1, '2018-10-18 12:00:00'), (2, 1, '2018-10-19 12:00:00');")
 
 # Adding racecards
 cursor.execute("INSERT INTO racecard (race_card_id, race_id, snail_id) "
-               "VALUES (1, 6, 1), (2, 6, 2), (3, 6, 3), "
-               "       (7, 1, 2), (8, 1, 6), (9, 1, 5), "
+               "VALUES (7, 1, 2), (8, 1, 6), (9, 1, 5), "
                "        (4, 2, 4), (5, 2, 5), (6, 2, 1);")
 
 # Adding raceresults
 cursor.execute("INSERT INTO raceresult (race_result_id, race_id, snail_id, position) "
-               "VALUES (1000, 6, 1, 1), (2000, 6, 2, 2), (3000, 6, 3, 3), "
-               "       (7000, 1, 2, 3), (8000, 1, 6, 2), (9000, 1, 5, 1);")
+               "VALUES (7000, 1, 2, 3), (8000, 1, 6, 2), (9000, 1, 5, 1);")
 
 # commit all changes to the DB
 db.commit()
