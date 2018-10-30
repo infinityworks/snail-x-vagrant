5b473629ef8ad49530128b5e980fc23d9d32b693
matt-twomey-iw <matt.twomey@gmail.com>
2018-10-30 17:27:11 +0000

    A scoore can now be calculated! Now need to save to db

--- inflight_insert_db.py
+++ inflight_insert_db.py
@@ -5,7 +5,8 @@ cursor = db.cursor()
 
 # create user and user score tables
 cursor.execute("INSERT INTO round (round_id, prize, start_date, closed, round_name) "
-               "VALUES (1, 100, '2018-10-17', false , 'Second Round');")
+               "VALUES (1, 100, '2018-10-17', false , 'First Round'),"
+               "(2, 100, '2019-10-19', false, 'Second Round');")
 
 # Adding trainers
 cursor.execute("INSERT INTO trainers (trainer_id, name) "
@@ -24,12 +25,23 @@ cursor.execute("INSERT INTO race (race_id, round_id, race_date) "
 
 # Adding racecards
 cursor.execute("INSERT INTO racecard (race_card_id, race_id, snail_id) "
-               "VALUES (7, 1, 2), (8, 1, 6), (9, 1, 5), "
-               "        (4, 2, 4), (5, 2, 5), (6, 2, 1);")
+               "VALUES (7, 1, 1), (8, 1, 2), (9, 1, 3), (10, 1, 4), "
+               "        (5, 2, 1), (6, 2, 3);")
 
 # Adding raceresults
 cursor.execute("INSERT INTO raceresult (race_result_id, race_id, snail_id, position) "
                "VALUES (1000, 1, 4, 4), (2000, 1, 3, 3), (3000, 1, 2, 2), (4000, 1, 2, 2), (5000, 1, 1, 1);")
+
+# Adding users
+cursor.execute("INSERT INTO users (user_id, first_name, last_name, password, email)"
+               "VALUES (1000, 'Matt', 'Twomey', 'password', 'mtwomey@gmail.com'),"
+               "        (2000, 'James', 'Sheard', 'iluvmatt', 'jsheard@gmail.com'),"
+               "        (3000, 'Emily', 'Birch', 'carbsarelife', 'ebirch@gmail.com');")
+
+# Adding racepredictions
+cursor.execute("INSERT INTO racepredictions (race_prediction_id, race_id, user_id, snail_id, created)"
+               "VALUES (1, 1, 1000, 1, '2018-10-18 00:00:00'), (2, 2, 1000, 3, '2018-10-18 00:00:00'),"
+               "        (3, 1, 2000, 4, '2018-10-18 12:00:00'), (4, 2, 2000, 1, '2018-10-18 00:00:00');")
 
 # commit all changes to the DB
 db.commit()
